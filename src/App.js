271328c5b07733f34a809edd4626a34e2d50fa7d
mohammed <zeinaboy@gmail.com>
2024-02-04 15:34:03 +0000

    add react files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Background from './components/background'; 
+
+function App() {
+  return (
+    <>
+      <Background />
+    </>
+  );
+}
+export default App;
---
+++ src/components/background.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/background.css';
+import backgroundImg from '../images/musicimg.jpeg';
+
+function Background() {
+    return(
+        <div className='background'>
+            
+        </div>  
+    )
+
+}
+
+export default Background;
